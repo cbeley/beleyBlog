7db8d2f12694bc94a992f40df14fa4834d89744c
Chris Beley <chris.beley@gmail.com>
2023-03-03 17:46:02 -0800

    unused import.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,5 @@
 import Typography from 'typography';
 import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants';
-import verticalRhythm from 'compass-vertical-rhythm';
 
 import { darkAccent } from '~src/colors.module.css';
 
