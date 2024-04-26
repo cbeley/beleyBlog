6a84d94d90c9cffb60fb91fb8d6a08486cd4d77e
Chris Beley <chris.beley@gmail.com>
2024-04-25 18:32:50 -0700

    Quick home page updates.

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 
-import resumeLink from '~src/components/resume';
 import SocialIcons from '~src/components/SocialIcons';
 
 import * as styles from './styles.module.css';
@@ -24,26 +23,6 @@ export default () => {
             <p className={styles.introText}>
                 Welcome to Chris Beley&apos;s little corner of the internet
                 where I write about just about anything that interests me!
-            </p>
-            <p>
-                Unfortunately, I was impacted by the layoffs at Twitter. I plan
-                to start interviewing again soon, so feel free to{' '}
-                <a title="email" href="mailto:chris.beley+blog@gmail.com">
-                    get in touch
-                </a>{' '}
-                or{' '}
-                <a
-                    title="LinkedIn"
-                    href="https://www.linkedin.com/in/chris-beley"
-                >
-                    check me out on linkedIn
-                </a>{' '}
-                if you have something that may interest me. You can also
-                download a{' '}
-                <a href={resumeLink} title="Chris Beley's Resume">
-                    pdf version of my resume
-                </a>
-                .
             </p>
         </article>
     );
